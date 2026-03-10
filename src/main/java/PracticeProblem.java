960d9d58769c3c24142f6fe47973e3258d1b8f17
JackyLui1215 <343344461@tdsb.ca>
2026-03-10 14:57:41 +0000

    finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,42 @@
+/**
+ * File: Methods
+ * Author: Jacky Lui
+ * Date Last Modified: Mar 10, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		
 
 	}
 
 	//Create your functions here
+
+	public static int sum(int sum1, int sum2) {
+		int number;
+		number = (sum1 + sum2);
+		return number;
+
+
+	}
+
+	public static int difference(int diff1, int diff2) {
+		int number1;
+		number1 = (diff1 - diff2);
+		return number1;
+	}
+
+	public static double product(double prod1, double prod2){
+		double number2;
+		number2 = (prod1 * prod2);
+		return number2;
+	}
+
+	public static String removeFirst(String word) {
+		String word1;
+		word1 = word.substring(1);
+		return word1;
+	}
 
 }
